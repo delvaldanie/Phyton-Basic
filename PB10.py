537e6bd38afb5684e8e763357d333dd2b338dbcf
delvaldanie <danieldelvalloret@gmail.com>
2026-05-20 12:56:51 +0200

    Ejercicios

--- PB10.py
+++ PB10.py
@@ -2,12 +2,10 @@
 # edades = [12, 17, 18, 20, 15, 30, 22]
 # Use filter() para quedarse solo con los mayores de edad.
 # Use map() para sumar uno acada edad 
-def filtrar_adulto(edad):
-    if edad >= 18:
-        return edad
+
 
 edades = [12, 17, 18, 20, 15, 30, 22]
-lista_filtrada = list(filter(filtrar_adulto, edades))
+lista_filtrada = list(filter(lambda edad: edad >= 18, edades))
 lista_sumada = list(map(lambda x: x+1, edades))
 print(lista_filtrada)
 print(lista_sumada)
